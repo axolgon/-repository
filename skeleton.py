b260a3a4c4405206ffdbc103365179ce5bde9e2f
남요셉 <130026424+axolgon@users.noreply.github.com>
2023-05-11 21:21:51 +0900

    Update skeleton.py

--- skeleton.py
+++ skeleton.py
@@ -6,54 +6,37 @@ from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import Header
 import cv2
 
-
-def apply_filter(input_, filter_):
-    input_height= input_.shape[0]
-    input_width = input_.shape[1]
-    filter_height= filter_.shape[0]
-    filter_width = filter_.shape[1]
-
-
-    output_height = input_height-filter_height+1
-    output_width = input_width-filter_width+1
-    output = np.zeros((output_height, output_width))
-
-    for i in range(output_height):
-        for j in range(output_width):
-            k=np.sum(filter_*input_[i:i+filter_height,j:j+filter_width])
-            if np.abs(k)>50:
-                output[i,j]=255
-            else:
-                output[i,j]=0
-
-    return output
-
-def BGR(img):
+def BGR(img, arr):
   bgr=[0,0,0]
   musigi='0'
-  for i in range(80):
+
+  for i in range(70):
+    p1=arr[0]+(arr[1]-arr[0])*(i/70)
+    p2=arr[3]+(arr[2]-arr[3])*(i/70)
     for j in range(100):
-      b,g,r=img[int((i/80)*img.shape[0]*16/27+img.shape[0]*5/42),int((j/100)*img.shape[1]*13/18+img.shape[1]*1/9)]
-      if (b>=180 and b<=255) and (r>=0 and r<=60):
-       bgr[0]+=1
-      elif (b>=0 and b<=60) and (g>=0 and g<=150) and (r>=190 and r<=255):
-       bgr[1]+=1
+      q=p1+(p2-p1)*(j/100)
+      b,g,r=img[int(q[1]),int(q[0])]
+      if (b>=170 and b<=255) and (g>=0 and g<=200):
+        bgr[0]+=1
+      elif (g>=0 and g<=150) and (r>=170 and r<=255):
+        bgr[1]+=1
       else:
-       bgr[2]+=1
+        bgr[2]+=1
   if np.argmax(bgr)==0:
-    musigi='1'
+    musigi='+1'
   elif np.argmax(bgr)==1:
-    musigi='-1'     
+    musigi='-1'    
   elif np.argmax(bgr)==2:
     musigi='0'    
   return musigi
-  
-x=np.array([[1,0,-1],[1,0,-1],[1,0,-1]])
+
 
 class DetermineColor:
     def __init__(self):
         self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.callback)
         self.color_pub = rospy.Publisher('/rotate_cmd', Header, queue_size=10)
+        #self.pub=rospy.Publisher('processed_data',np.ndarray,queue_size=10)
+        #self.last_published_data=None
         self.bridge = CvBridge()
         self.count=0
 
@@ -68,21 +51,50 @@ class DetermineColor:
             msg = data.header
             msg.frame_id = '0'  # default: STOP
 
-            #image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            #cv2.imshow('um',apply_filter(image_gray,x))
+
+            draw=image.copy()
+
+            image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+           
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (5, 5), 0)
+            edged = cv2.Canny(gray, 75, 200)
+
+        
+           
+            cnts, x = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:3]
+            global pts
+
+            for c in cnts :
+              peri = cv2.arcLength(c, True)
+              verticles=cv2.approxPolyDP(c, 0.02* peri, True)
+              if len(verticles) == 4:
+                      break
+            try:          
+                      if cv2.contourArea(c)>(draw.shape[0]*draw.shape[1])/4:
+                         pts = verticles.reshape(4, 2)
+                      else: 
+                         chamshipjo
+            except:
+                      pass
+                             
             
-            
+
+            cv2.imshow('um', draw)
+            cv2.waitKey(10)    
             # determine background color
             # TODO
             # determine the color and assing +1, 0, or, -1 for frame_id
             #msg.frame_id = '+1' # CCW (Blue background)
             # msg.frame_id = '0'  # STOP
             # msg.frame_id = '-1' # CW (Red background)
-            
 
-            
-            #print(image[int(image.shape[0]*0.5),int(image.shape[1]*0.3)])
-            msg.frame_id=BGR(image)
+
+            msg.frame_id=BGR(draw, pts)
+
+
 
             # publish color_state
             self.color_pub.publish(msg)
@@ -91,11 +103,13 @@ class DetermineColor:
             print(e)
 
 
+
     def rospy_shutdown(self, signal, frame):
         rospy.signal_shutdown("shut down")
         sys.exit(0)
 
 if __name__ == '__main__':
+    pts=np.array([[0,0],[0,0],[0,0],[0,0]])
     rospy.init_node('CompressedImages1', anonymous=False)
     detector = DetermineColor()
     rospy.spin()
